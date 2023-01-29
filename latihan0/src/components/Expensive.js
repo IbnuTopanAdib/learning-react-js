845dcd90f25724b2ea3a31891ffe8de785f76c16
IbnuTopanAdib <resyaderesya@gmail.com>
2023-01-29 10:11:55 +0700

    learning react day 1

---
+++ latihan0/src/components/Expensive.js
@@ -0,0 +1,14 @@
+function Expensive(){
+    return(
+        <div>
+            
+            <h1>Hallo Kucing</h1>
+            <div>
+                <p>kucing adalah makhluk beraki empat dan merupakan hewan vertebrata</p>
+                <span>kucing juga dikenal sebagai dewa di mesir kuno</span>
+            </div>
+        </div>
+    );
+}
+
+export default Expensive;
